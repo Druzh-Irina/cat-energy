fe5b1fafeb69599ea257619be592ccfb407ce97d
IrinaDruzhinina <imdruzh@gmail.com>
2020-12-31 06:01:40 +0300

    правки

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,3 +1,4 @@
+(function () {
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -11,4 +12,5 @@
       navMain.classList.add('main-nav--closed');
       navMain.classList.remove('main-nav--opened');
     }
-  });
+  })
+})();
